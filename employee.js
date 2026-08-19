f83a25183b82f0aeccb935c46a87b0513bf32b78
im-nub-123 <sonar.vaibhav.2309@gmail.com>
2026-08-19 13:46:07 +0530

    Create employee.js

---
+++ employee.js
@@ -0,0 +1,10 @@
+let name = "Ravi Kumar";
+let age = 28;
+let permanentEmployee = true;
+let skills = ["JavaScript", "HTML", "CSS"];
+
+console.log("Employee Information:");
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Permanent Employee: " + (permanentEmployee ? "Yes" : "No"));
+console.log("Skills: " + skills.join(", "));
