3693dad37ddf93af3439197a2e36c24664aada32
im-nub-123 <sonar.vaibhav.2309@gmail.com>
2026-08-19 13:56:40 +0530

    Create gym.js

---
+++ gym.js
@@ -0,0 +1,27 @@
+let day = 7;
+
+switch (day) {
+    case 1:
+        console.log("Day 1: Chest Day");
+        break;
+    case 2:
+        console.log("Day 2: Back Day");
+        break;
+    case 3:
+        console.log("Day 3: Leg Day");
+        break;
+    case 4:
+        console.log("Day 4: Shoulder Day");
+        break;
+    case 5:
+        console.log("Day 5: Biceps Day");
+        break;
+    case 6:
+        console.log("Day 6: Triceps Day");
+        break;
+    case 7:
+        console.log("Day 7: Rest Day");
+        break;
+    default:
+        console.log("Invalid day! Please enter a number between 1 and 7.");
+}
